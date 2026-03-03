56629743aa75635370cd1ad57ef528e039119592
anasnaeemqu <136015453+anasnaeemqu@users.noreply.github.com>
2026-03-04 03:18:27 +0500

    Change static file serving to use app.get

--- server/static.ts
+++ server/static.ts
@@ -23,7 +23,7 @@ export function serveStatic(app: Express) {
   app.use(express.static(distPath));
 
   // Fall through to index.html for client-side routing
-  app.use("/{*path}", (_req, res) => {
-    res.sendFile(path.resolve(distPath, "index.html"));
-  });
+ app.get("*", (_req, res) => {
+  res.sendFile(path.resolve(distPath, "index.html"));
+});
 }
